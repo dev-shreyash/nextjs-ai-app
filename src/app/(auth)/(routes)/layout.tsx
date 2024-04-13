88f1e686172dd427f652639202ae137ad1f67682
Shreyash-69 <bhosaleshreyash2@gmail.com>
2024-04-13 17:08:25 +0530

    user auth setup

---
+++ src/app/(auth)/(routes)/layout.tsx
@@ -0,0 +1,12 @@
+
+const AuthLayot =({
+    children
+}:{
+    children:React.ReactNode
+})=>{
+    <div className="flex items-center justify-center">
+        {children}
+    </div>
+}
+
+export default AuthLayot
